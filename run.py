8b8bf7a9b44440710cac126135ca353d46fb568d
rurakite <ruravictor@gmail.com>
2023-03-09 14:51:06 +0000

    Add loop to keep asking for data until that will entered correctly

--- run.py
+++ run.py
@@ -19,14 +19,21 @@ def get_sales_data():
     """
     Get sales figures input from the user.
     """
-    print("Please enter sales data from the last market.")
-    print("Data should be six numbers, separated by commas.")
-    print("Examle: 10,25,34,16,28,20\n")
+    while True:
+        print("Please enter sales data from the last market.")
+        print("Data should be six numbers, separated by commas.")
+        print("Examle: 10,25,34,16,28,20\n")
 
-    data_str = input("Enter your data here: ")
+        data_str = input("Enter your data here: ")
+        
+        sales_data = data_str.split(",")
+        validate_data(sales_data)
+
+        if validate_data(sales_data):
+            print("Data is valid!")
+            break
+
     
-    sales_data = data_str.split(",")
-    validate_data(sales_data)
 
 
 def validate_data(values):
@@ -34,12 +41,16 @@ def validate_data(values):
     Validating the users input.
     """
     try:
+        [int(value) for value in values]
         if len(values) != 6:
             raise ValueError(
                 f"Exactly six values required, you provided {len(values)}"
             )
     except ValueError as e:
         print(f"Invalid data: {e}, please try again.\n")
+        return False
+
+    return True
 
 
 get_sales_data()
